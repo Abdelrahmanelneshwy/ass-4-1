ef72f1fe8827640b237317fed40509f23f6a42f0
Abdelrahmanelneshwy <110162529+Abdelrahmanelneshwy@users.noreply.github.com>
2022-09-11 07:24:36 +0200

    Delete counter directory

--- counter/BIT_MATH.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MATH_H
-#define _MATH_H
-
-#define SET_BIT(REG, BITNO)  (REG) |= (1 << (BITNO))
-#define CLR_BIT(REG, BITNO)  (REG) &= (~(1 << (BITNO)))
-#define TOG_BIT(REG, BITNO)  (REG) ^= (1 << (BITNO))
-#define GET_BIT(REG, BITNO)  (((REG) >> (BITNO)) & 0x01)
-
-#endif
